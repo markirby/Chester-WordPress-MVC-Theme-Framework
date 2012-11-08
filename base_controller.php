834870af95b339ea1ca322db9b1dfd68a6ff7898
Mark Kirby <mark@thisishatch.co.uk>
2012-11-08 15:47:12 +0000

    Added license.

--- base_controller.php
+++ base_controller.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @author   	Mark Kirby
+ * @copyright	Copyright (c) 2012, Mark Kirby, http://mark-kirby.co.uk/
+ * @license  	http://en.wikipedia.org/wiki/MIT_License The MIT License
+ * @package  	Chester
+ * @version  	0.1
+ * @link     	https://github.com/markirby/Chester-WordPress-MVC-Theme-Framework
+ * @link     	http://thisishatch.co.uk/
+ */
+ 
 require_once(dirname(__FILE__).'/lib/mustache/Autoloader.php');
 require_once(dirname(__FILE__).'/lib/mustache/Compiler.php');
 require_once(dirname(__FILE__).'/lib/mustache/Context.php');
--- wp_core_data_helpers.php
+++ wp_core_data_helpers.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @author   	Mark Kirby
+ * @copyright	Copyright (c) 2012, Mark Kirby, http://mark-kirby.co.uk/
+ * @license  	http://en.wikipedia.org/wiki/MIT_License The MIT License
+ * @package  	Chester
+ * @version  	0.1
+ * @link     	https://github.com/markirby/Chester-WordPress-MVC-Theme-Framework
+ * @link     	http://thisishatch.co.uk/
+ */
+ 
 class ChesterWPCoreDataHelpers {
   
   public static function getBlogInfoData() {
